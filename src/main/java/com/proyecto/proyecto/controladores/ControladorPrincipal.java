651310ff75aa0e26b8a1a50789f5bff25acf6baf
Sergio Sotil <sergiosotil78@hotmail.com>
2026-01-27 20:19:46 -0500

    Subir archivos del proyecto

---
+++ src/main/java/com/proyecto/proyecto/controladores/ControladorPrincipal.java
@@ -0,0 +1,70 @@
+package com.proyecto.proyecto.controladores;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ControladorPrincipal {
+    private List<Producto> listaProductos = new ArrayList<>();
+    private List<Producto> ofertas = new ArrayList<>();
+
+    public ControladorPrincipal() {
+        listaProductos.add(new Producto("CB-001", "Mocasín Elegante", "Caballero", "Cuero Legítimo", 180.00, "https://oechsle.vteximg.com.br/arquivos/ids/21256873-800-800/imageUrl_1.jpg?v=638844427504570000"));
+        listaProductos.add(new Producto("CB-002", "Stiletto Nude", "Dama", "Gamuza", 145.00, "https://calimodpruebaio.vtexassets.com/arquivos/ids/254826/7YT00700025_1-zapato-de-vestir-estileto-con-taco-aguja-color-nude.jpg?v=638512388037000000"));
+        listaProductos.add(new Producto("CB-003", "Bota de Cuero Premium", "Dama", "Cuero", 210.00, "https://oechsle.vteximg.com.br/arquivos/ids/16866428/image-0.jpg?v=638363847528000000"));
+        listaProductos.add(new Producto("CB-004", "Zapatilla Urbana", "Unisex", "Lona", 95.00, "https://plazavea.vteximg.com.br/arquivos/ids/31181508-418-418/image-019b5f07537e4612adb19789254a45d3.jpg"));
+        ofertas.add(new Producto("CB-005", "Zapatilla Basica", "Caballero", "Cuero", 100.00, "https://vialepe.vtexassets.com/arquivos/ids/190962/ZAPATILLA-0-CASUAL-HOMBRE-CUERO-VIALE-HOMME-FLAT-4-RICARDO-025-MARRON-1_1000px-1.jpg?v=638544040861800000"));
+        ofertas.add(new Producto("CB-006", "Stiletto Viale", "Dama", "Cuero", 150.00, "https://vialepe.vtexassets.com/arquivos/ids/191824/ZAPATO-STILETTO-VESTIR-DAMA-CUERO-VIALE-TACO-7-HER-2411-VISON-1.jpg?v=638544042037000000"));
+        ofertas.add(new Producto("CB-007", "Bota Pauli", "Dama", "Sintético", 80.00, "https://d3fvqmu2193zmz.cloudfront.net/items_2/uid_commerces.1/uid_items_2.FDC9GFJT1TAO/500x500/65C62D6324F92-Bota-Dama-B-Pauli.webp"));
+        ofertas.add(new Producto("CB-008", "Zapato Estelar", "Caballero", "Cuero", 145.00, "https://greenbay.pe/cdn/shop/files/ESTELAR_-_3024G-NEGRO_1.webp?v=1746816605"));
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/catalogo")
+    public String catalogo(Model model) {
+        model.addAttribute("productos", listaProductos);
+        return "catalogo";
+    }
+
+    @GetMapping("/ofertas")
+    public String ofertas(Model model) {
+        model.addAttribute("productos", ofertas); 
+        return "ofertas";
+    }
+
+    @GetMapping("/nosotros")
+    public String nosotros() {
+        return "nosotros";
+    }
+
+    @GetMapping("/contacto")
+    public String contacto() {
+        return "contacto";
+    }
+
+    public class Producto {
+        public String codigo;
+        public String nombre;
+        public String categoria;
+        public String material;
+        public double precio;
+        public String imagen;
+
+        public Producto(String codigo, String nombre, String categoria, String material, double precio, String imagen) {
+            this.codigo = codigo;
+            this.nombre = nombre;
+            this.categoria = categoria;
+            this.material = material;
+            this.precio = precio;
+            this.imagen = imagen;
+        }
+    }
+}
